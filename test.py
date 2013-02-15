da0c4433ba9be9cb57c1f1a1db8eb3ed4eb450d5
Naveen Michaud-Agrawal <naveenm@enthought.com>
2013-02-15 11:57:38 -0500

    More complicated test

--- test.py
+++ test.py
@@ -1,8 +1,13 @@
 
+def func2(arg):
+    print arg
+    return arg + 5
+
 def main():
     x = 5
     x += 6
-    print x
+    val = func2(x)
+    print x, val
 
 if __name__ == '__main__':
     main()
